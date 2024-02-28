0b9c27943d5985062b573596cca6568ba54471b9
Arthur-Allen <82613934+Arthur-All@users.noreply.github.com>
2024-02-28 20:13:23 +0000

    ++

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # Project-C
+
+## Usage:
+
+### Development
+
+To start the development environment, run:
+
+```bash
+npm run dev
+```
+
+## This command will bring up necessary services using Docker Compose and start the Next.js development server.
+
+## Services Management
+
+- Start services:
+
+```bash
+npm run services:up
+```
+
+- Stop services:
+
+```bash
+npm run services:stop
+```
+
+- Remove services:
+
+```bash
+npm run services:down
+```
+
+---
+
+## Linting
+
+To check and fix linting issues, you can use the following commands:
+
+- Check linting:
+
+```bash
+npm run lint:check
+```
+
+- Fix linting issues:
+
+```bash
+npm run lint:fix
+```
+
+---
+
+## Testing
+
+To run tests, execute:
+
+```bash
+npm test
+```
+
+For watching mode during development, you can use:
+
+```bash
+npm run test:watch
+```
---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,32 @@
+import database from "infra/database.js";
+
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionValue = dbMaxConnectionResult.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count;
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionValue),
+        opened_connections: dbOpenedConnectionsValue,
+      },
+    },
+  });
+}
+
+export default status;
